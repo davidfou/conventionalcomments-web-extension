9abdd098f740a4c3f6dd8cc746f4d92fa5aef19b
David Fournier <fr.david.fournier@gmail.com>
2022-06-19 19:10:41 +0200

    Add global information

--- src/popup/Components/Layout/index.tsx
+++ src/popup/Components/Layout/index.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import LayoutComponent from "./Layout";
+import Intro from "../Intro";
 import ConfigureDomains from "../ConfigureDomains";
 
 function Layout(): JSX.Element {
   return (
     <LayoutComponent>
+      <Intro />
       <ConfigureDomains />
     </LayoutComponent>
   );
