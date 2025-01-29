a8a37dbe07764392194e07559efcda21d9b34e81
David Fournier <fr.david.fournier@gmail.com>
2025-01-29 00:06:50 +0000

    Update GitHub e2e test selectors

--- tests/MainPage/GitLabPage/index.ts
+++ tests/MainPage/GitLabPage/index.ts
@@ -164,24 +164,20 @@ class GitLabPage extends AbstractPage {
     await this.getNoteEditSelector(threadId, commentId).click();
   }
 
-  async editCommentFromMainPage(
-    messageId: string,
-    pageType?: "pullRequestDescription" | "issueDescription"
-  ) {
-    switch (pageType) {
-      case "pullRequestDescription":
-        await this.page.goto(
-          config.get<string>("e2e.gitlab.editPullRequestPage")
-        );
-        break;
-      case "issueDescription":
-        await this.page.click(
-          "button[aria-label='Edit title and description']"
-        );
-        break;
-      default:
-        await this.getNoteEditSelector("", messageId).click();
-    }
+  async editMainCommentFromPullRequestPage() {
+    await this.page.goto(config.get<string>("e2e.gitlab.editPullRequestPage"));
+  }
+
+  async editCommentFromPullRequestPage(noteId: string): Promise<void> {
+    await this.getNoteEditSelector("", noteId).click();
+  }
+
+  async editMainCommentFromIssuePage() {
+    await this.page.goto(config.get<string>("e2e.gitlab.issuePage"));
+  }
+
+  async editCommentFromIssuePage(noteId: string) {
+    await this.getNoteEditSelector("", noteId).click();
   }
 
   getReplyInputLocator(threadId: string) {
